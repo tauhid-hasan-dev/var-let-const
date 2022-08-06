9b24dd49b40d11442cdeb4b4fef9db92922a3ac4
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-08-06 10:25:12 +0300

    first commit

---
+++ var-let-const.js
@@ -0,0 +1,17 @@
+//var-let-const
+
+//global scope
+var x = 1;
+let y = 2;
+const z = 3;
+
+//local scope
+
+/* 
+Inside a local scope there is two scope 
+1- block scope
+2- funtion scope 
+*/
+
+
+
