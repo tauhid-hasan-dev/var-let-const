4819197184a8cc9cf152495eead2b703fdedbbb4
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-08-06 11:31:58 +0300

    var is function scoped and let and const are blocked scope

--- var-let-const.js
+++ var-let-const.js
@@ -5,13 +5,10 @@
 //1-we have access to the global scope variable in anywhere in the file
 //2- global variable can not be access from the up 
 
-console.log(x); // output will be undefined;
+//console.log(x); // output will be undefined;
 var x = 1;
 let y = 2;
 const z = 3;
-
-
-
 
 
 /* =============local scope================ */
@@ -28,16 +25,52 @@ Inside a local scope there is two scope
 
 //FUNCTION SCOPE 
 function functionScope() {
-    console.log('Inside a function called function scope')
+    //console.log('Inside a function called function scope')
     //nested block scope inside a function
     //nested blocks can be if statements, for loops, switch statements
     {
         let x = 4;
-        console.log(x);
+        //console.log(x);
     }
 }
-
 functionScope();
+
+
+/*================== SUMMERY =====================*/
+
+/* inside a funtion scope
+var is a function scoped
+let and const is a block scoped */
+
+//global scope
+
+var a = 1;
+let b = 2;
+const c = 3;
+
+console.log(`global: ${a}`);
+console.log(`global: ${b}`);
+console.log(`global: ${c}`);
+
+function myFunc() {
+    var a = 10;
+    const b = 20;
+    {
+        var a = 55; //function scope (it can be accessed from anywhere in the function)
+        const b = 99; //let and const is block scopeed 
+        console.log(`block: ${a}`);
+        console.log(`block: ${b}`);
+        console.log(`block: ${c}`);
+    }
+    console.log(`function: ${a}`);
+    console.log(`function: ${b}`);
+    console.log(`function: ${c}`);
+}
+
+myFunc();
+
+
+
 
 
 
